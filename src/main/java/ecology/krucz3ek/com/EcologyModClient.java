6858e501fd7ddd721fb5643532ec25fb303d6784
Kruk <65093601+krucz3ek@users.noreply.github.com>
2025-11-15 23:48:09 +0100

    Initial commit

---
+++ src/main/java/ecology/krucz3ek/com/EcologyModClient.java
@@ -0,0 +1,10 @@
+package ecology.krucz3ek.com;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EcologyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
